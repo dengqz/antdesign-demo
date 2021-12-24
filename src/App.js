239d897b2b61859c166bf54cfb256a0d5a6cd277
qizhi.deng <qizhi.deng@yaduo.com>
2021-12-24 15:59:19 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';//引入
+class App extends React.Component{
+  render(){
+    return (
+        <div className="App">
+          <div>
+            <Button type="primary">Primary</Button>//使用
+          </div>
+        </div>
+    )
+  }
+}
+export default App;
